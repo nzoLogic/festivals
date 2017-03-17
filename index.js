f6f5d7975329d63b6082937fb5ec284156c00ffc
nzoLogic <nzoLogic@gmail.com>
2017-03-17 16:44:29 -0700

    typo fixed

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from 'express';
 let app = express();
 let db = require('./db');
 
-
+//api routes for festivals
 app.get('/api/festivals', (req, res) => {
   let festivals = db.Festival.find({}, (err, festivals) => err? console.log("error in db") : res.json(festivals))
 })
 
+//redirect all other routes to default
 app.get('/*', (req, res) => {
   res.send('hello')
 })
