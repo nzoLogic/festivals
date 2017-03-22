1a0200f898d5fbe6b8591fe9fe20af85ae9d5dbd
nzoLogic <nzoLogic@gmail.com>
2017-03-22 14:31:42 -0400

    enabled server to serve static files for heroku

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 let app = express();
 let db = require('./db');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 //api routes for festivals
 app.get('/api/festivals', (req, res) => {
   let festivals = db.Festival.find({}, (err, festivals) => err? console.log("error in db") : res.json(festivals))
