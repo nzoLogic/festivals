d39814abc285e89d6b630883fd7d3df8187e3b46
nzoLogic <nzoLogic@gmail.com>
2017-04-04 11:20:04 -0700

    removed unused components

--- client/src/Festival.js
+++ client/src/Festival.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class FestivalRow extends Component {
   render() {
--- client/src/Festivals.js
+++ client/src/Festivals.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import Client from './Client'
 import FestivalRow from './Festival'
-import { Route } from 'react-router-dom'
 
 class Festivals extends Component {
   constructor(props){
